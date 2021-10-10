c8179fbbdc37dc2475da43af12d2b9cf499c8822
Frederick Javalera <frederickjavalera@gmail.com>
2021-10-10 11:52:51 -0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Frederick Javalera on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
